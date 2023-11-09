1779c5a87ada9b05471d8f67db5b3721dd0701fd
golffer420014 <142491843+golffer420014@users.noreply.github.com>
2023-11-09 23:05:48 +0700

    golf
    แก้ admin เรื่องรูปภาพที่เครื่องอื่นไม่เห็น
    event
    category

--- backend/routers/event.js
+++ backend/routers/event.js
@@ -1,11 +1,36 @@
 const { Event } = require('../models/event')
 const express = require('express');
 const router = express.Router();
+const multer = require('multer')
+const fsUpdate = require('fs').promises;
+const fsDelete = require('fs');
+const path = require('path');
 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+};
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
 
+        if (isValid) {
+            uploadError = null;
+        }
+        // cb = callback
+        cb(uploadError, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        //ทุกพื้นที่ว่างจะถูกเติมด้วย - เช่น 'golf suriya' จะเป็น 'golf-suriya'
+        const fileName = file.originalname.split(' ').join('-');
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`);
+    }
+})
 
-
-
+const uploadOptions = multer({ storage: storage })
 
 
 
@@ -24,12 +49,18 @@ router.get(`/`, async (req, res) => {
 
 
 //post
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
 
     try{
+        const file = req.file;
+        if (!file) return console.log('No image uploaded');
+
+        const fileName = file.filename;
+        const localhost = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
         let event = new Event({
             // image: req.body.image,
-            image: req.body.image,
+            image: `${localhost}${fileName}`,
 
         });
 
@@ -37,29 +68,43 @@ router.post(`/`, async (req, res) => {
 
 
 
-        res.send(event);
+        // res.send(event);
     }catch(err){
         res.status(500).send('The Event cannot be created');
     }
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
-        Event.findByIdAndRemove(req.params.id)
-            .then(event => {
-                if (event) {
-                    return res.status(200).json({ success: true, message: 'Image Event is delete' })
-                } else { 
-                    return res.status(404).json({ success: false, message: 'Image Event is delete' })
+        const event = await Event.findById(req.params.id);
+        if (event) {
+            // Assuming the image property contains a URL like 'http://10.0.2.2:3000/public/uploads/image.png'
+            // You need to extract the file path after the domain.
+            const urlParts = new URL(event.image);
+            const filePath = path.join('public', 'uploads', path.basename(urlParts.pathname));
+            // First, delete the image file
+            fsDelete.unlink(filePath, async (err) => {
+                if (err) {
+                    // If the file does not exist, log the error but still try to remove the event from the database
+                    console.error(err);
                 }
-            })
-    } catch (err) { 
-        console.log(err)
-        return res.status(404).json({ success: false, error: err })
+                // Whether the image was deleted or not, try to remove the event
+                try {
+                    await Event.findByIdAndRemove(req.params.id);
+                    return res.status(200).json({ success: true, message: 'ลบผู้ใช้และไฟล์รูปภาพเรียบร้อย' });
+                } catch (err) {
+                    console.error(err);
+                    return res.status(500).json({ success: false, message: 'ไม่สามารถลบผู้ใช้ได้' });
+                }
+            });
+        } else {
+            return res.status(404).json({ success: false, message: 'ไม่พบผู้ใช้' });
+        }
+    } catch (err) {
+        return res.status(500).json({ success: false, error: err });
     }
-
-})
+});
 
 
 
